06d89eb1fddec205222b60c2573fa38bf1c762ea
Yasu <ychihiro0315@icloud.com>
2025-06-04 12:44:13 +0900

    poster0また

--- src/components/poster0.vue
+++ src/components/poster0.vue
@@ -1,12 +1,23 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col v-for="(index, name) in poster_name" :key="index" cols="12" md="4">
+      <v-col
+        v-for="(name, index) in poster_name"
+        :key="index"
+        cols="12"
+        md="4"
+      >
         <v-card>
-          <v-img :src="baseUrl + poster_path[index]" v-if="poster_name"></v-img>
-          <v-card-text v-else>
-            画像を読み込み中です...
-          </v-card-text>
+          <template v-if="poster_path[index]">
+            <v-img
+              :src="baseUrl + poster_path[index]"
+              height="200px"
+            ></v-img>
+            <v-card-title>{{ name }}</v-card-title>
+          </template>
+          <template v-else>
+            <v-card-text>画像を読み込み中です...</v-card-text>
+          </template>
         </v-card>
       </v-col>
     </v-row>
@@ -30,11 +41,11 @@ export default {
   },
   methods: {
     async fetch_latest_posters() {
-      try{
+      try {
         const response = await axios.get('https://raspberrypi.tail8fca5.ts.net/posters_info/');
         this.poster_name = response.data.name;
         this.poster_path = response.data.urls;
-      } catch(error) {
+      } catch (error) {
         console.error('画像の取得に失敗しました:', error)
       }
     }
